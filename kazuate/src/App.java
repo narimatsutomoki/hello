2779fb0932b2c263403b2b806d63b4b3dc3c9967
Your Name <you@example.com>
2021-06-15 13:25:52 +0900

    commit

--- kazuate/src/App.java
+++ kazuate/src/App.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.io.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("数当てゲームです。答えは2桁の自然数です。");
-        Scanner scan = new Scanner(System.in);
         int num_ans = 53;
         int num_chance = 5;
         int num_in = 0;
         while(num_chance>0){
+            BufferedReader d=new BufferedReader(new InputStreamReader(System.in));
             while(true){
+                System.out.println("残りチャンス:"+num_chance);
+                String s;
+                s=d.readLine();
                 try{
-                    System.out.println("残りチャンス:"+num_chance);
-                    num_in = scan.nextInt();
-                    break;
-                }catch (InputMismatchException e){
-                    System.out.println("数字を入力してください");
-                    scan.next();
+                    num_in=Integer.parseInt(s);
                 }
+                catch(java.lang.NumberFormatException e){
+                    System.out.println("数字を入力してください");
+                    continue;
+                }
+                if(num_in>9 && num_in<100) break;
+                System.out.println("2桁の自然数を入力してください");
             }
+            
             if (num_in == num_ans){
                 System.out.println("当たり");
                 System.exit(0);
@@ -39,6 +43,5 @@ public class App {
             num_chance-=1;
         }
         System.out.println("残りチャンスが0となったので、失敗です。。");
-        scan.close();
     }
 }
